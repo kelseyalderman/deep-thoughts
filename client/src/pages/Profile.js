25dafef252f225a29836b64d1d3a54c9e6207deb
Kelsey Alderman <kelseyalderman@gmail.com>
2022-06-20 16:48:36 -0700

    create thought from component and clear form upon submit

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Navigate, useParams } from "react-router-dom";
-
 import ThoughtList from "../components/ThoughtList";
 import FriendList from "../components/FriendList";
-
-import { useQuery } from "@apollo/client";
+import { ADD_FRIEND } from "../utils/mutations";
+import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
-
 import Auth from "../utils/auth";
+import ThoughtForm from "../components/ThoughtForm";
 
 const Profile = () => {
   const { username: userParam } = useParams();
@@ -17,6 +16,18 @@ const Profile = () => {
   });
 
   const user = data?.me || data?.user || {};
+
+  const [addFriend] = useMutation(ADD_FRIEND);
+
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   // navigate to personal profile page if username is the logged-in user's
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
@@ -42,6 +53,12 @@ const Profile = () => {
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : "your"} profile.
         </h2>
+
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -60,6 +77,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
